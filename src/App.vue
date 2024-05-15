75ae5c9ac08f50998a6c6a24b9d40471f50c165a
septiaheskaa <heskaseptia@gmail.com>
2024-05-15 19:02:06 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <UserPosts />
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </div>
+</template>
+
